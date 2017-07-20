746c2635f9958c9a25ee389d995159f584bb9958
Tomas Petras Rupsys <tprupsys@gmail.com>
2017-07-20 21:39:34 +0300

    Initial commit with webpack dev server

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+var path = require("path");
+
+module.exports = {
+    entry: ['./app/index.js'],
+    output: {
+        path: path.resolve(__dirname, "build"),
+        publicPath: "/assets/",
+        filename: "bundle.js"
+    }
+};
